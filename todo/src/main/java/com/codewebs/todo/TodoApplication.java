9a957079f62f5956ca53696411bd5330a772925a
stevenmcgirr <stevenmcgirr@192.168.0.74>
2021-01-17 23:00:30 +0000

    Excluded tomcat

--- todo/src/main/java/com/codewebs/todo/TodoApplication.java
+++ todo/src/main/java/com/codewebs/todo/TodoApplication.java
@@ -1,16 +1,14 @@
 package com.codewebs.todo;
 
-import org.apache.logging.log4j.Logger;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.codewebs.todo" })
@@ -25,14 +23,14 @@ public class TodoApplication extends SpringBootServletInitializer {
 		SpringApplication.run(TodoApplication.class, args);
 	}
 
-	@Bean
-	public InternalResourceViewResolver getViewResolver() {
-		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-		viewResolver.setPrefix("/WEB-INF/jsp/");
-		// viewResolver.setPrefix("/resources/css/");
-		viewResolver.setSuffix(".jsp");
-		// viewResolver.setSuffix(".css");
-		return viewResolver;
-	}
+//	@Bean
+//	public InternalResourceViewResolver getViewResolver() {
+//		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+//		viewResolver.setPrefix("/WEB-INF/jsp/");
+//		// viewResolver.setPrefix("/resources/css/");
+//		viewResolver.setSuffix(".jsp");
+//		// viewResolver.setSuffix(".css");
+//		return viewResolver;
+//	}
 
 }
